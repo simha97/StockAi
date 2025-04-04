99d80b7b64e3ab1c6ca86bc5f605d994dc337636
simha97 <62260118+simha97@users.noreply.github.com>
2025-04-04 14:07:51 +0200

    fix CloudFlare

--- stockAi/index.js
+++ stockAi/index.js
@@ -1,5 +1,4 @@
 import { dates } from '/utils/dates'
-
 const tickersArr = []
 
 const generateReportBtn = document.querySelector('.generate-report-btn')
@@ -37,30 +36,61 @@ const loadingArea = document.querySelector('.loading-panel')
 const apiMessage = document.getElementById('api-message')
 
 async function fetchStockData() {
-    document.querySelector('.action-panel').style.display = 'none'
-    loadingArea.style.display = 'flex'
-    try {
-        const stockData = await Promise.all(tickersArr.map(async (ticker) => {
-            const url = `https://api.polygon.io/v2/aggs/ticker/${ticker}/range/1/day/${dates.startDate}/${dates.endDate}?apiKey=${process.env.POLYGON_API_KEY}`
-            const response = await fetch(url)
-            const data = await response.text()
-            const status = await response.status
-            if (status === 200) {
-                apiMessage.innerText = 'Creating report...'
-                return data
-            } else {
-                loadingArea.innerText = 'There was an error fetching stock data.'
-            }
-        }))
-        fetchReport(stockData.join(''))
-    } catch(err) {
-        loadingArea.innerText = 'There was an error fetching stock data.'
-        console.error('error: ', err)
-    }
+  document.querySelector(".action-panel").style.display = "none";
+  loadingArea.style.display = "flex";
+  try {
+    const stockData = await Promise.all(
+      tickersArr.map(async (ticker) => {
+        const url = `https://polygon-api-worker.simon-hallak-3.workers.dev/?ticker=${ticker}&startDate=${dates.startDate}&endDate=${dates.endDate}`;
+        const response = await fetch(url);
+
+        if (!response.ok) {
+          const errMsg = await response.text();
+          throw new Error("Worker error: " + errMsg);
+        }
+        apiMessage.innerText = "Creating report...";
+        return response.text();
+      })
+    );
+    fetchReport(stockData.join(""));
+  } catch (err) {
+    loadingArea.innerText = "There was an error fetching stock data.";
+    console.error(err.message);
+  }
 }
 
 async function fetchReport(data) {
-    /** AI goes here **/
+  const messages = [
+    {
+      role: "system",
+      content:
+        "You are a trading guru, but you are also Borat. Given data on share prices over the past 3 days, write a report of no more than 100 words describing the stocks performance and recommending whether to buy, hold or sell.",
+    },
+    {
+      role: "user",
+      content: data,
+    },
+  ];
+
+  try {
+    const url = "https://openai-api-worker.simon-hallak-3.workers.dev/";
+    const response = await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(messages),
+    });
+    const data = await response.json();
+
+    if(!response.ok){
+        throw new Error(`Worker Error: ${data.error}`);
+    }
+    renderReport(data.content)
+  } catch (err) {
+    console.log("Error:", err);
+    loadingArea.innerText = "Unable to access AI. Please refresh and try again";
+  }
 }
 
 function renderReport(output) {
